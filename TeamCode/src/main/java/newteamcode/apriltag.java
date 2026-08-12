2527dd77883c20e4eb1e802636af2768c8ebfaaa
Alchemist <allenmaximilian03@gmail.com>
2026-08-12 13:30:22 +0700

    Update apriltag.java

--- TeamCode/src/main/java/newteamcode/apriltag.java
+++ TeamCode/src/main/java/newteamcode/apriltag.java
@@ -1,8 +1,8 @@
 package newteamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -10,26 +10,50 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
+
 @TeleOp(name = "apriltag", group = "TeleOp")
-public class apriltag  extends OpMode {
+public class apriltag extends OpMode {
 
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
 
     @Override
     public void init() {
-
-        aprilTag = new AprilTagProcessor.Builder()
-                .build();
+        aprilTag = new AprilTagProcessor.Builder().build();
 
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .addProcessor(aprilTag)
+                .enableLiveView(true)
+                .setAutoStopLiveView(false)
                 .build();
-    }
-    @Override
-    public void loop(){
 
+        FtcDashboard.getInstance().startCameraStream(visionPortal, 0);
+    }
+
+    @Override
+    public void loop() {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+
+        telemetry.addData("Tag Detected", detections.size());
+
+        for (AprilTagDetection detection : detections) {
+            telemetry.addData("ID", detection.id);
+            if (detection.ftcPose != null) {
+                telemetry.addData("X (inch)", detection.ftcPose.x);
+                telemetry.addData("Y (inch)", detection.ftcPose.y);
+                telemetry.addData("Yaw (deg)", detection.ftcPose.yaw);
+            }
+        }
+
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        if (visionPortal != null) {
+            visionPortal.close();
+        }
     }
 
     public List<AprilTagDetection> getDetections() {
