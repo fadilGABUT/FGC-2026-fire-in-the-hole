e56ca6f645da87bf9d47d3079c085abf4a16b59e
Alchemist <allenmaximilian03@gmail.com>
2026-08-18 19:42:49 +0700

    Update apriltagVision.java

--- TeamCode/src/main/java/newteamcode/apriltagVision.java
+++ TeamCode/src/main/java/newteamcode/apriltagVision.java
@@ -3,8 +3,11 @@ package newteamcode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
@@ -16,6 +19,8 @@ public class apriltagVision {
 
     public void init(HardwareMap hardwareMap) {
         aprilTag = new AprilTagProcessor.Builder()
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
                 .build();
 
         visionPortal = new VisionPortal.Builder()
@@ -34,12 +39,14 @@ public class apriltagVision {
             telemetry.addData("ID", detection.id);
 
             if (detection.ftcPose != null) {
-                telemetry.addData("X", "%.2f", detection.ftcPose.x);
-                telemetry.addData("Y", "%.2f", detection.ftcPose.y);
-                telemetry.addData("Z", "%.2f", detection.ftcPose.z);
-                telemetry.addData("Yaw", "%.2f", detection.ftcPose.yaw);
-                telemetry.addData("Pitch", "%.2f", detection.ftcPose.pitch);
-                telemetry.addData("Roll", "%.2f", detection.ftcPose.roll);
+                telemetry.addData("X", "%.2f in", detection.ftcPose.x);
+                telemetry.addData("Y", "%.2f in", detection.ftcPose.y);
+                telemetry.addData("Z", "%.2f in", detection.ftcPose.z);
+                telemetry.addData("Yaw", "%.2f deg", detection.ftcPose.yaw);
+                telemetry.addData("Pitch", "%.2f deg", detection.ftcPose.pitch);
+                telemetry.addData("Roll", "%.2f deg", detection.ftcPose.roll);
+            } else {
+                telemetry.addData("Pose Status", "ftcPose is NULL");
             }
         }
     }
