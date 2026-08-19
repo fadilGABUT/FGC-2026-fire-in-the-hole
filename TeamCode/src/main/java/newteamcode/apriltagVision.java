57d6251ca8aa1f8a6bea635e6692b15b8b8072aa
Alchemist <allenmaximilian03@gmail.com>
2026-08-19 08:48:44 +0700

    Apriltag vision FGC2026 lbrary

--- TeamCode/src/main/java/newteamcode/apriltagVision.java
+++ TeamCode/src/main/java/newteamcode/apriltagVision.java
@@ -7,10 +7,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
+import global.first.IgnitingInnovationGameDatabase;
 
 public class apriltagVision {
 
@@ -19,8 +19,8 @@ public class apriltagVision {
 
     public void init(HardwareMap hardwareMap) {
         aprilTag = new AprilTagProcessor.Builder()
-                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
+                .setTagLibrary(IgnitingInnovationGameDatabase.getIgnitingInnovationTagLibrary())
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
                 .build();
 
         visionPortal = new VisionPortal.Builder()
@@ -36,17 +36,22 @@ public class apriltagVision {
         telemetry.addData("Tags Detected", detections.size());
 
         for (AprilTagDetection detection : detections) {
-            telemetry.addData("ID", detection.id);
-
-            if (detection.ftcPose != null) {
-                telemetry.addData("X", "%.2f in", detection.ftcPose.x);
-                telemetry.addData("Y", "%.2f in", detection.ftcPose.y);
-                telemetry.addData("Z", "%.2f in", detection.ftcPose.z);
-                telemetry.addData("Yaw", "%.2f deg", detection.ftcPose.yaw);
+            if (detection.metadata != null && detection.ftcPose != null) {
+                telemetry.addData("ID", detection.id);
+                telemetry.addData("Name", detection.metadata.name);
+                telemetry.addData("X", "%.2f cm", detection.ftcPose.x);
+                telemetry.addData("Y", "%.2f cm", detection.ftcPose.y);
+                telemetry.addData("Z", "%.2f cm", detection.ftcPose.z);
                 telemetry.addData("Pitch", "%.2f deg", detection.ftcPose.pitch);
                 telemetry.addData("Roll", "%.2f deg", detection.ftcPose.roll);
+                telemetry.addData("Yaw", "%.2f deg", detection.ftcPose.yaw);
+                telemetry.addData("Range", "%.2f cm", detection.ftcPose.range);
+                telemetry.addData("Bearing", "%.2f deg", detection.ftcPose.bearing);
+                telemetry.addData("Elevation", "%.2f deg", detection.ftcPose.elevation);
             } else {
-                telemetry.addData("Pose Status", "ftcPose is NULL");
+                telemetry.addData("ID Unknown", detection.id);
+                telemetry.addData("Center X", detection.center.x);
+                telemetry.addData("Center Y", detection.center.y);
             }
         }
     }
